4c105b945b6f26591ff9f76d1020b1e105ae0b09
kusn <serjikf@yandex.ru>
2021-04-01 07:24:27 +0300

    Начал выполнение ДЗ

---
+++ Home_work1/Home_work1/Main.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+
+void solution1();
+void solution2();
+void solution3();
+void menu();
+
+
+int main(int argc, const char* argv[])
+{
+	int sel = 0;
+	do
+	{
+		menu();
+		scanf("%i", &sel);
+		switch (sel)
+		{
+		case 1:
+			solution1();
+			break;
+		case 2:
+			solution2();
+			break;
+		case 3:
+			solution3();
+			break;
+		case 0:
+			printf("Bye-bye");
+			break;
+		default:
+			printf("Wrong selected\n");
+		}
+	} while (sel != 0);
+
+
+	return 0;
+}
+
+void solution1()
+{
+	printf("Solution 1\n");
+	// Решение
+}
+void solution2()
+{
+	printf("Solution 2\n");
+	// Решение
+}
+void solution3()
+{
+	printf("Solution 3\n");
+	// Решение
+}
+void menu()
+{
+	printf("1 - task1\n");
+	printf("2 - task2\n");
+	printf("3 - task3\n");
+	printf("0 - exit\n");
+}
