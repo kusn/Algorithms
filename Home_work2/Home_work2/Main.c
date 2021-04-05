035c4298eded1dd2c75b5dff1d29f00ac01b144c
Kusn <serjikf@yandex.ru>
2021-04-05 19:23:33 +0300

    Выполнил ДЗ

--- Home_work2/Home_work2/Main.c
+++ Home_work2/Home_work2/Main.c
@@ -16,9 +16,12 @@ char buffer[12];
 void solution1();
 void solution2();
 void solution3();
+void solution4();
 void menu();
 void DecToBin(int n);
 void StrRev(char* S);
+int MyPow(int a, int b);
+int MyBinPow(int a, int b);
 
 
 int main(int argc, char* argv[])
@@ -42,6 +45,9 @@ int main(int argc, char* argv[])
 		case 3:
 			solution3();
 			break;
+		case 4:
+			solution4();
+			break;
 		case 0:
 			printf("Bye-bye");
 			break;
@@ -53,6 +59,7 @@ int main(int argc, char* argv[])
 	return 0;
 }
 
+// Задача 1. Перевод числа из десятичной системы в двоичную
 void solution1()
 {
 	int n;
@@ -69,23 +76,71 @@ void solution1()
 	StrRev(buffer);		//Переворачиваем результат
 	result = atoi(buffer);	//Преобразуем строку в целое
 	printf("%i", result);
-	printf("\n");
+	printf("\n\n");
 }
+
+// Задача 2. Реализовать функцию возведения числа a в степень b:
+// a. без рекурсии;
 void solution2()
 {
-	printf("Solution 2\n");
+	int a, b, i, res = 1;
+
+	printf("Задача 2. Реализовать функцию возведения числа a в степень b без рекурсии\n");
+	
 	// Решение
+	printf("Введите целое положительное число: ");
+	scanf("%i", &a);
+	printf("Введите степень (целое положительное число): ");
+	scanf("%i", &b);
+	for (i = 1; i <= b; i++)
+		res = res * a;
+
+	printf("Результат возведения числа %i в степень %i: %i", a, b, res);
+	printf("\n\n");
 }
+
+// b. рекурсивно;
 void solution3()
 {
-	printf("Solution 3\n");
+	int a, b, res;
+
+	printf("Задача 2. Реализовать функцию возведения числа a в степень b рекурсивно\n");
+	
 	// Решение
+	printf("Введите целое положительное число: ");
+	scanf("%i", &a);
+	printf("Введите степень (целое положительное число): ");
+	scanf("%i", &b);
+	res = MyPow(a, b);
+	printf("Результат возведения числа %i в степень %i: %i", a, b, res);
+	printf("\n\n");
+
 }
+
+// b. рекурсивно, используя свойство четности степени;
+void solution4()
+{
+	int a, b, res;
+
+	printf("Задача 2. Реализовать функцию возведения числа a в степень b рекурсивно, используя свойство четности степени\n");
+
+	// Решение
+	printf("Введите целое положительное число: ");
+	scanf("%i", &a);
+	printf("Введите степень (целое положительное число): ");
+	scanf("%i", &b);
+	res = MyBinPow(a, b);
+	printf("Результат возведения числа %i в степень %i: %i", a, b, res);
+	printf("\n\n");
+
+}
+
 void menu()
 {
-	printf("Задача 1. Перевод числа из десятичной системы в двоичную\n");
-	printf("2 - task2\n");
-	printf("3 - task3\n");
+	printf("1 - Перевод числа из десятичной системы в двоичную\n");
+	printf("2 - Реализовать функцию возведения числа a в степень b без рекурсии\n");
+	printf("3 - Реализовать функцию возведения числа a в степень b рекурсивно\n");
+	printf("4 - Реализовать функцию возведения числа a в степень b рекурсивно, используя свойство четности степени\n");
 	printf("0 - exit\n");
 }
 
@@ -116,5 +171,26 @@ void StrRev(char* S)
 		S[i] = S[j];
 		S[j] = t;
 		i++; j--;
+	}
+}
+
+//Возведение в степень рекурсивно
+int MyPow(int a, int b)
+{		
+	if (b > 0) return a * MyPow(a, b - 1);
+	return 1;
+}
+
+//Возведение в степень рекурсивно, используя свойство четности степени
+int MyBinPow(int a, int b)
+{
+	if (b == 0)
+		return 1;
+	if (b % 2 == 1)
+		return MyBinPow(a, b - 1) * a;
+	else
+	{
+		int c = MyBinPow(a, b / 2);
+		return c * c;
 	}
 }
