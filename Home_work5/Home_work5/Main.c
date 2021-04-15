df4414fd2c650de841018a2a1bd5ed037a06b9ff
Kusn <serjikf@yandex.ru>
2021-04-15 09:57:15 +0300

    Выполнил третью задачу

--- Home_work5/Home_work5/Main.c
+++ Home_work5/Home_work5/Main.c
@@ -16,7 +16,9 @@
 #include <stdio.h>
 #include <malloc.h>
 #include <locale.h>
+#include <stdlib.h>
 #define T int
+#define TT char
 
 int result = 0;
 char buffer[12];
@@ -28,7 +30,11 @@ void menu();
 void push(T value);
 T pop();
 void PrintStack();
+void push2(TT value);
+TT pop2();
+void PrintStack2();
 void DecToBin(int n);
+int IsBracketSequence(char* s, int size);
 
 // Опишем структуру узла списка
 struct TNode
@@ -37,7 +43,14 @@ struct TNode
 	struct TNode* next;
 };
 
+struct TTNode
+{
+	TT value;
+	struct TTNode* next;
+};
+
 typedef struct TNode Node;
+typedef struct TTNode Node2;
 
 struct Stack
 {
@@ -45,8 +58,16 @@ struct Stack
 	int size;
 	int maxSize;
 };
-struct Stack Stack;
 
+struct Stack2
+{
+	Node2* head;
+	int size;
+	int maxSize;
+};
+
+struct Stack Stack;
+struct Stack2 Stack2;
 
 
 int main()
@@ -113,14 +134,29 @@ void solution2()
 }
 void solution3()
 {
-	printf("Solution 3\n");
+	char s[30];
+
+	Stack2.maxSize = 30;
+	Stack2.head = NULL;
+	Stack2.size = 0;
+	printf("Задача 3.  Написать программу, которая определяет, является ли введенная скобочная последовательность правильной\n");
+	
 	// Решение
+	printf("Введите скобочную последовательность. Например: (2 + (2 * 2)) или[2 / {5 * (4 + 7)}]\n");
+	scanf("%s", s);
+	
+	if (IsBracketSequence(s, 30))
+		printf("Скобочная последовательность верна\n");
+	else
+		printf("Скобочная последовательность не верна\n");
+	printf("\n");
+
 }
 void menu()
 {
 	printf("1 - Задача 1. Реализовать перевод из десятичной в двоичную систему счисления с использованием стека\n");
 	printf("2 - Задача 2. Добавить в программу «реализация стека на основе односвязного списка» проверку на выделение памяти\n");
-	printf("3 - task3\n");
+	printf("3 - Задача 3.  Написать программу, которая определяет, является ли введенная скобочная последовательность правильной\n");
 	printf("0 - exit\n");
 }
 
@@ -135,6 +171,19 @@ void push(T value)
 	tmp->next = Stack.head;
 	Stack.head = tmp;
 	Stack.size++;
+}
+
+void push2(TT value)
+{
+	if (Stack2.size >= Stack2.maxSize) {
+		printf("Ошибка. Стек переполнен!\n");
+		return;
+	}
+	Node2* tmp = (Node2*)malloc(sizeof(Node2));
+	tmp->value = value;
+	tmp->next = Stack2.head;
+	Stack2.head = tmp;
+	Stack2.size++;
 }
 
 T pop() {
@@ -157,12 +206,42 @@ T pop() {
 	return value;
 }
 
+TT pop2() {
+	if (Stack2.size == 0)
+	{
+		printf("Stack is empty");
+		return;
+	}
+	// Временный указатель
+	Node2* next = NULL;
+	// Значение "наверху" списка
+	TT value;
+	value = Stack2.head->value;
+	next = Stack2.head;
+	Stack2.head = Stack2.head->next;
+	// Запись, на которую указывала голова удаляем, освобождая память
+	free(next);
+	// Возвращаем значение, которое было в голове
+	Stack2.size--;
+	return value;
+}
+
 void PrintStack()
 {
 	Node* current = Stack.head;
 	while (current != NULL)
 	{
 		printf("%i", current->value);
+		current = current->next;
+	}
+}
+
+void PrintStack2()
+{
+	Node2* current = Stack2.head;
+	while (current != NULL)
+	{
+		printf("%c", current->value);
 		current = current->next;
 	}
 }
@@ -175,4 +254,34 @@ void DecToBin(int n)
 		push(result);
 		n = n / 2;
 	}
+}
+
+int IsBracketSequence(char* s, int size)
+{
+	int r = 0;
+	char c;
+
+	for (int i = 0; i < size; i++)
+	{
+		if (Stack2.size != 0)
+			r = 0;
+		if (s[i] == '(' || s[i] == '[' || s[i] == '{')
+		{
+			push2(s[i]);
+		}
+		else if (s[i] == ')' || s[i] == ']' || s[i] == '}')
+		{
+			c = pop2();
+			if (s[i] == ')' && c == '(')
+				r = 1;
+			else if (s[i] == ']' && c == '[')
+				r = 1;
+			else if (s[i] == '}' && c == '{')
+				r = 1;
+			else
+				r = 0;
+		}
+	}
+
+	return r;
 }
