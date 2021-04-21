7dbe2858c26beb79934935e2e58be08a77c27b9e
Kusn <serjikf@yandex.ru>
2021-04-21 16:25:18 +0300

    Выполнил 1 задачу

--- Home_work7/Home_work7/Main.c
+++ Home_work7/Home_work7/Main.c
@@ -5,14 +5,29 @@
 // 3. Написать функцию обхода графа в ширину.
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <malloc.h>
+
+#define infinity 9999;
+#define MAX 6;
+
+int size = MAX;
+const char* filename = "..\\..\\adjacencymatrix.txt";
 
 void solution1();
 void solution2();
 void solution3();
+int** GetAdjacencyMatrix(char* file, int size);		// Получаем матрицу из файла
+void PrintMatrix(int** matrix, int size);			// Печатаем матрицу
 void menu();
+
 int main()
 {
 	int sel = 0;
+
+	setlocale(LC_ALL, "rus");
+
 	do
 	{
 		menu();
@@ -39,8 +54,18 @@ int main()
 }
 void solution1()
 {
-	printf("Solution 1\n");
+	int** G;
+	
+	printf("Задача 1. Написать функции, которые считывают матрицу смежности из файла и выводят ее на экран\n");
+	
 	// Решение
+	G = (int**)malloc(size * sizeof(int*));
+	for (int i = 0; i < size; i++)
+		G[i] = (int*)malloc(size * sizeof(int));
+
+	G = GetAdjacencyMatrix(filename, size);
+	PrintMatrix(G, size);
+	printf("\n");
 }
 void solution2()
 {
@@ -54,8 +79,46 @@ void solution3()
 }
 void menu()
 {
-	printf("1 - task1\n");
+	printf("1 - Задача 1. Написать функции, которые считывают матрицу смежности из файла и выводят ее на экран\n");
 	printf("2 - task2\n");
 	printf("3 - task3\n");
 	printf("0 - exit\n");
+}
+
+// Получаем матрицу из файла
+int** GetAdjacencyMatrix(char* filename, int size)
+{
+	int tmp = 0;
+	int** G;
+
+	G = (int**)malloc(size * sizeof(int*));
+	for (int i = 0; i < size; i++)
+		G[i] = (int*)malloc(size * sizeof(int));
+
+	FILE* file = fopen(filename, "r");
+	if (file == NULL)
+	{
+		printf("Файл %s не найден", filename);
+		exit(1);
+	}
+	for (int i = 0; i < size; i++)
+		for (int j = 0; j < size; j++)
+		{
+			fscanf(file, "%i", &tmp);
+			G[i][j] = tmp;
+		}
+	fclose(file);
+	
+	return G;
+}
+
+// Печатаем матрицу
+void PrintMatrix(int** matrix, int size)
+{
+	for (int i = 0; i < size; i++)
+	{
+		for (int j = 0; j < size; j++)
+			printf("%4i", matrix[i][j]);
+		printf("\n");
+	}
 }
